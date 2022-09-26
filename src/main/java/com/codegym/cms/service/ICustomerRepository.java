4d7c161f770c237214749d71bb2f9636b36347a0
LENOVO <imbakito1604@gmail.com>
2022-09-26 14:45:33 +0700

    hoan thien bai thuc hanh

--- src/main/java/com/codegym/cms/service/ICustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.codegym.cms.service;
-
-import com.codegym.cms.model.Customer;
-import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ICustomerRepository extends PagingAndSortingRepository<Customer, Long> {
-}
